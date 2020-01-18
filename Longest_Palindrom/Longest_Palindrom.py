7f6fc86be493471a1d05e9f2c4003ee4093d4b18
nsahadat <m.n.sahadat@gmail.com>
2020-01-18 12:49:05 -0600

    no message

--- Longest_Palindrom/Longest_Palindrom.py
+++ Longest_Palindrom/Longest_Palindrom.py
@@ -3,6 +3,23 @@
 Created on Sat Jan 18 11:55:08 2020
 
 @author: mnsah
+
+A palindrome is a sequence of characters that reads the same backwards and forwards. Given a string, s, find the longest palindromic substring in s.
+
+Example:
+Input: "banana"
+Output: "anana"
+
+Input: "million"
+Output: "illi"
+class Solution: 
+    def longestPalindrome(self, s):
+      # Fill this in.
+        
+# Test program
+s = "tracecars"
+print(str(Solution().longestPalindrome(s)))
+# racecar
 """
 
 class Solution: 
